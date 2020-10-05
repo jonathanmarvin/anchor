d43657b4a01bd19eb8c76d543761dc0a02271ca1
RM <67162767+testandtrack@users.noreply.github.com>
2020-10-05 18:45:39 +0100

    added __ to the constructor!

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-public function _construct()
+public function __construct()
         {
 
-        parent::_construct();
+        parent::__construct();
         $this->load->model('homes');
         }
 
